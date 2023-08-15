7dca203963b28e14b5ed48c0d710a57fc3ce9756
Jovan Maric <33927662+maric993@users.noreply.github.com>
2023-08-15 22:11:50 +0200

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='petlja_sphinx_extensions',
+    version='0.1',
+    description='PetljaSphinxExtensions',
+    author='Petlja',
+    packages=find_packages(),
+    install_requires=[
+        'sphinx',
+    ],
+)
