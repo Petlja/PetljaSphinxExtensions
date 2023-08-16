087c865380e9a25090fc0bdcfa07a2db3ffac337
Jovan Maric <33927662+maric993@users.noreply.github.com>
2023-08-16 14:24:32 +0200

    readme

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
+    python_requires=">=3.7",
+    version='1.0.0',
+    author="Fondacija Petlja",
+    author_email="team@petlja.org",
+    description="Petlja's sphinx extensions for e-learning content",
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    zip_safe=False,
     name='petlja_sphinx_extensions',
     version='0.1',
-    description='PetljaSphinxExtensions',
-    author='Petlja',
     packages=find_packages(),
     install_requires=[
         'sphinx',
